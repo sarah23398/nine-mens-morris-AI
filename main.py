afdb444d2c9cc0f1f95d06fadd7eedf58f9f9328
sarah23398 <sarah23398@yahoo.ca>
2022-04-02 21:38:11 -0400

    Code cleanup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
 
 #function for printing the current state of the board, takes a double array as input for the map
-#empty spots are represented by 0, player 1 and 2's pieces are represented by 1 and 2
-def print_map(map):
+#empty spots are represented by x, player 1 and 2's pieces are represented by 1 and 2
+def print_map(map):    
     print(str(map[0])+"-----"+str(map[1])+"-----"+str(map[2]))
     print("|     |     |")
     print("| "+str(map[3])+"---"+str(map[4])+"---"+str(map[5])+" |")
@@ -21,158 +21,99 @@ def print_map(map):
     print(str(map[21])+"-----"+str(map[22])+"-----"+str(map[23]))
 
 #function called during player1's turn during phase one, placing pieces
-def player1_turn1(map,move):
-    if(map[int(move)]!=0):#the piece cannot be placed because a piece is already there
-        print("invalid input")
+def player1_phase1(map,move):
+    if(map[int(move)]!='x'):#the piece cannot be placed because a piece is already there
+        print("Invalid input: position is taken.")
         print_map(map)
         move = input()
-        player1_turn1(map,move)
+        player1_phase1(map,move)
     else:
-        print("player 1 made a move on "+move)
+        print("Player 1 made a move on "+move)
         map[int(move)] = 1
         print_map(map)
 
-def player2_turn1(map,move):
-    if(map[int(move)]!=0):
-        print("invalid input")
+def player2_phase1(map,move):
+    if(map[int(move)]!='x'):
+        print("Invalid input: position is taken.")
         print_map(map)
         move = input()
-        player2_turn1(map,move)
+        player2_phase1(map,move)
     else:
-        print("player 2 made a move on "+move)
+        print("Player 2 made a move on "+move)
         map[int(move)] = 2
         print_map(map)
 
 #function to check if the specified player has a line on the board
+verticals = [[0, 9, 21], [3, 10, 18], [6, 11, 15], [1, 4, 7], [16, 19, 22], [8, 12, 17], [5, 13, 20], [3, 15, 23]]
 def check_line(map,player):
     line = False
-    if(map[0]==player and map[1]==player and map[2]==1):
-        line = True
-    if(map[3]==player and map[4]==player and map[5]==player):
-        line = True
-    if(map[6]==player and map[7]==player and map[8]==player):
-        line = True
-    if(map[9]==player and map[10]==player and map[11]==player):
-        line = True
-    if(map[12]==player and map[13]==player and map[14]==player):
-        line = True
-    if(map[15]==player and map[16]==player and map[17]==player):
-        line = True
-    if(map[18]==player and map[19]==player and map[20]==player):
-        line = True
-    if(map[21]==player and map[22]==player and map[23]==player):
-        line = True
+    for i in range(0, 21, 3):
+        if (map[i] == player and map[i + 1] == player and map[i + 2] == player):
+            line = True
+            break
 
-    if(map[0]==player and map[9]==player and map[21]==player):
-        line = True
-    if(map[3]==player and map[10]==player and map[18]==player):
-        line = True
-    if(map[6]==player and map[11]==player and map[15]==player):
-        line = True
-    if(map[1]==player and map[4]==player and map[7]==player):
-        line = True
-    if(map[16]==player and map[19]==player and map[22]==player):
-        line = True
-    if(map[8]==player and map[12]==player and map[17]==player):
-        line = True
-    if(map[5]==player and map[13]==player and map[20]==player):
-        line = True
-    if(map[3]==player and map[15]==player and map[23]==player):
-        line = True
+    for v in verticals:
+        if all(map[i] == player for i in v):
+            line = True
+            break
+
     return line
 
 #function for a specified player to remove their opponent's piece
 def remove_piece(map, move, player):
     if(map[move]==1 and player==2):
-        print("player 2 removed player 1's piece at "+str(move))
-        map[move] = 0
+        print("Player 2 removed Player 1's piece at "+str(move))
+        map[move] = 'x'
         print_map(map)
     elif(map[move]==2 and player==1):
-        print("player 1 removed player 2's piece at "+str(move))
-        map[move] = 0
+        print("Player 1 removed Player 2's piece at "+str(move))
+        map[move] = 'x'
         print_map(map)
     else:
-        print("invalid move")
+        print("Invalid move")
         print_map(map)
         move = input()
         remove_piece(map, int(move), player)
 
 #function to check if a specified piece forms a line
 def make_line(map, move):
-    if(map[move]==0)return False
+    if(map[move]=='x'): return False
     remove = True
-    if(move==0 or move==1 or move==2):
-        if(map[0]==map[1] and map[1]==map[2]):
-            remove = False
-    if(move==3 or move==4 or move==5):
-        if(map[3]==map[4] and map[4]==map[5]):
-            remove = False
-    if(move==6 or move==7 or move==8):
-        if(map[6]==map[7] and map[7]==map[8]):
-            remove = False
-    if(move==9 or move==10 or move==11):
-        if(map[9]==map[10] and map[10]==map[11]):
-            remove = False
-    if(move==12 or move==13 or move==14):
-        if(map[12]==map[13] and map[13]==map[14]):
-            remove = False
-    if(move==15 or move==16 or move==17):
-        if(map[15]==map[16] and map[16]==map[17]):
-            remove = False
-    if(move==18 or move==19 or move==20):
-        if(map[18]==map[19] and map[19]==map[20]):
-            remove = False
-    if(move==21 or move==22 or move==23):
-        if(map[21]==map[22] and map[22]==map[23]):
-            remove = False
+    for i in range(0, 21, 3):
+        if move == i or move == i + 1 or move == i + 2:
+            if map[i] == map[i + 1] and map[i] == map[i + 2]:
+                remove = False
+                break
 
-    if(move==0 or move==9 or move==21):
-        if(map[0]==map[9] and map[9]==map[21]):
-            remove = False
-    if(move==3 or move==10 or move==18):
-        if(map[3]==map[10] and map[10]==map[18]):
-            remove = False
-    if(move==6 or move==11 or move==15):
-        if(map[6]==map[11] and map[11]==map[15]):
-            remove = False
-    if(move==1 or move==4 or move==7):
-        if(map[1]==map[4] and map[4]==map[7]):
-            remove = False
-    if(move==16 or move==19 or move==22):
-        if(map[16]==map[19] and map[19]==map[22]):
-            remove = False
-    if(move==8 or move==12 or move==17):
-        if(map[8]==map[12] and map[12]==map[17]):
-            remove = False
-    if(move==5 or move==13 or move==20):
-        if(map[5]==map[13] and map[13]==map[20]):
-            remove = False
-    if(move==3 or move==15 or move==23):
-        if(map[3]==map[15] and map[15]==map[23]):
-            remove = False
+    for v in verticals:
+        if move in v:
+            if map[v[0]] == map[v[1]] and map[v[0]] == map[v[2]]:
+                remove = False
+                break
+            
     return not remove
 
-map = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+map = ['x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x','x']
 
 player1_moves = 9
 player2_moves = 9
 while(player1_moves!=0 and player2_moves!=0):
-    print("it is player 1's turn")
+    print("Player 1's turn:")
     move = input()
-    player1_turn1(map,move)
+    player1_phase1(map,move)
     player1_moves-=1
     if(check_line(map,1)):
-        print("player 1 can remove a piece")
+        print("Player 1 can remove a piece:")
         move = input()
         remove_piece(map,int(move),1)
 
-    print("it is player 2's turn")
+    print("Player 2's turn:")
     move = input()
-    player2_turn1(map,move)
+    player2_phase1(map,move)
     player2_moves-=1
     if(check_line(map,2)):
-        print("player 2 can remove a piece")
+        print("Player 2 can remove a piece:")
         move = input()
         remove_piece(map,int(move),2)
-print("final map: ")
+print("Final map: ")
 print_map(map)
